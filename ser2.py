5ee3b7791dd44510aa6610eb6ddaf4da633ac3bd
allbear <allbear826@gmail.com>
2021-01-24 11:52:33 +0900

    停止関数追加

--- ser2.py
+++ ser2.py
@@ -13,12 +13,12 @@ class Servo():
       self.servo3_pwm=1500
    
    def go_ahed(self):
-      self.pi.set_servo_pulsewidth(self.servo1, 1800)
+      self.pi.set_servo_pulsewidth(self.servo1, 1600)
       self.pi.set_servo_pulsewidth(self.servo2, 1400)
       self.pi.set_servo_pulsewidth(self.servo3, 1400)
    
    def go_back(self):
-      self.pi.set_servo_pulsewidth(self.servo1, 1200)
+      self.pi.set_servo_pulsewidth(self.servo1, 1400)
       self.pi.set_servo_pulsewidth(self.servo2, 1600)
       self.pi.set_servo_pulsewidth(self.servo3, 1600)
    
@@ -26,6 +26,10 @@ class Servo():
       self.pi.set_servo_pulsewidth(self.servo1, 1700)
       self.pi.set_servo_pulsewidth(self.servo2, 1400)
       self.pi.set_servo_pulsewidth(self.servo3, 1400)
+   def stop(self):
+      self.pi.set_servo_pulsewidth(self.servo1, 1500)
+      self.pi.set_servo_pulsewidth(self.servo2, 1500)
+      self.pi.set_servo_pulsewidth(self.servo3, 1500)
  
    def main(self):
       self.pi.set_mode(self.servo1, pigpio.OUTPUT)
@@ -35,7 +39,9 @@ class Servo():
          self.go_ahed()
          time.sleep(3)
          self.go_back()
-         time.sleep(3)    
+         time.sleep(3)
+         self.stop()
+         time.sleep(3)  
       self.pi.stop()
 servo = Servo()
 servo.main()
